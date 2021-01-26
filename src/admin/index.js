edacc6c16fad61ae4fdfe84bdc9df11726d863e2
Arturo Galan <arturo.galan@clarity.ai>
2021-01-26 10:37:15 +0100

    added plugin

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,5 +1,4 @@
-import CMS, { init } from 'netlify-cms'
+import CMS from 'netlify-cms'
 import NetlifyCmsWidgetI18n from 'netlify-cms-widget-i18n'
-const {controlComponent, previewComponent} = NetlifyCmsWidgetI18n;
-console.log('ehmmm')
-CMS.registerWidget('i18n', controlComponent, previewComponent)
+
+CMS.registerWidget('i18n', I18nControl, I18nPreview)
